713d24560c207085a164fa6039009dacc842fc99
papadanku <115061077+papadanku@users.noreply.github.com>
2023-02-21 19:21:42 -0700

    Update findAITemplates.py

--- findAITemplates.py
+++ findAITemplates.py
@@ -6,6 +6,7 @@
 import os
 
 # File information
+
 filePathDict = {
     ".ai": list(),
     ".tweak": list()
@@ -21,33 +22,12 @@ aiDict = {
     "templates": list()
 }
 
-# Functions
-def addTemplates(line):
-    for keyWord in aiDict["keywords"]:
-        if keyWord in line.lower():
-            aiTemplate = line.split(" ")[1]
-            aiDict["templates"].append(aiTemplate)
-
-def isValidTemplate(line):
-    for aiTemplate in aiDict["templates"]:
-        if aiTemplate in line:
-            return True
-    return False
-
-def findTemplates(filePath):
-    with open(filePath, "r") as file:
-        for line in file:
-            if (".aiTemplate" not in line) or ("rem" in line):
-                continue
-            if isValidTemplate(line) is False:
-                aiTemplate = line.split(" ")[-1]
-                string = f"{filePath}\n{aiTemplate}"
-                print(string)
-
 # Main operations
+
 objectPath = "D:\Program Files (x86)\Project Reality\Project Reality BF2\mods\pr_repo\objects"
 
 # [1] Get all AI and object files
+
 for root, dir, files, in os.walk(objectPath):
     for fileName in files:
         for extension in filePathDict.keys():
@@ -57,11 +37,32 @@ for root, dir, files, in os.walk(objectPath):
                 filePathDict[extension].append(realFilePath)
 
 # [2] Get created aiTemplates
+
+def addTemplates(line):
+    for keyWord in aiDict["keywords"]:
+        if keyWord in line.lower():
+            aiTemplate = line.split(" ")[1]
+            aiDict["templates"].append(aiTemplate)
+
 for path in filePathDict[".ai"]:
     with open(path, "r") as file:
         for line in file:
             addTemplates(line.strip())
 
 # [3] See if aiTemplate in file exists
+
+def isValidTemplate(line):
+    for aiTemplate in aiDict["templates"]:
+        if aiTemplate in line:
+            return True
+    return False
+
 for filePath in filePathDict[".tweak"]:
-    findTemplates(filePath)
+    with open(filePath, "r") as file:
+        for line in file:
+            if (".aiTemplate" not in line) or ("rem" in line):
+                continue
+            if isValidTemplate(line) is False:
+                aiTemplate = line.split(" ")[-1]
+                string = f"{filePath}\n{aiTemplate}"
+                print(string)

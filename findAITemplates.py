43ef8dc7e9242ba8098c796d85c2d17cb45c8a29
papadanku <115061077+papadanku@users.noreply.github.com>
2023-02-23 19:56:37 -0700

    Update findAITemplates.py

--- findAITemplates.py
+++ findAITemplates.py
@@ -7,7 +7,10 @@ import os
 
 # File information
 
-extensions = { ".ai", ".tweak" }
+extensions = {
+    ".ai",
+    ".tweak"
+}
 
 filePathDict = {
     ".ai": set(),
@@ -24,11 +27,9 @@ templateKeyWords = {
 aiTemplates = set()
 
 # Main operations
-
 objectPath = "D:\Program Files (x86)\Project Reality\Project Reality BF2\mods\pr_repo\objects"
 
 # [1] Get all AI and object files
-
 for root, dir, files, in os.walk(objectPath):
     for fileName in files:
         fileExtension = os.path.splitext(fileName)[-1]
@@ -38,19 +39,14 @@ for root, dir, files, in os.walk(objectPath):
             filePathDict[fileExtension].add(realFilePath)
 
 # [2] Get created aiTemplates
-
-def addTemplates(line):
-    templateString = line.split(" ")
-    if templateString[0] in templateKeyWords:
-        aiTemplates.add(templateString[1])
-
-for path in filePathDict[".ai"]:
-    with open(path, "r") as file:
+for filePath in filePathDict[".ai"]:
+    with open(filePath, "r") as file:
         for line in file:
-            addTemplates(line.strip())
+            templateString = line.strip().split(" ")
+            if templateString[0] in templateKeyWords:
+                aiTemplates.add(templateString[1])
 
 # [3] See if aiTemplate in file exists
-
 for filePath in filePathDict[".tweak"]:
     with open(filePath, "r") as file:
         for line in file:
